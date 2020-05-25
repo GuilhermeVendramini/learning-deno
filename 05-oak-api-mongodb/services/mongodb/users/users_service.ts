b70e23fba8ad2711f31b4c264ccb53cf5733713b
Guilherme <guilhermevendra@gmail.com>
2020-05-25 14:54:38 -0300

    Deno crud api with mongodb

--- 05-oak-api-mongodb/services/mongodb/users/users_service.ts
+++ 05-oak-api-mongodb/services/mongodb/users/users_service.ts
@@ -3,27 +3,27 @@ import person from "../../../models/person_model.ts";
 
 const data = db.collection("users");
 
-async function findAllUsers(): Promise<any> {
+const findAllUsers = async (): Promise<any> => {
   return await data.find();
-}
+};
 
-async function insertUser(user: person): Promise<any> {
+const insertUser = async (user: person): Promise<any> => {
   return await data.insertOne(user);
-}
+};
 
-async function findUser(id: number): Promise<any> {
+const findUser = async (id: number): Promise<any> => {
   return await data.find({ id: id });
-}
+};
 
-async function updateUser(user: person): Promise<any> {
+const updateUser = async (user: person): Promise<any> => {
   return await data.updateOne(
     { id: user.id },
     { $set: { name: user.name, age: user.age } },
   );
-}
+};
 
-async function deleteUser(id: number): Promise<any> {
+const deleteUser = async (id: number): Promise<any> => {
   return await data.deleteOne({ id: id });
-}
+};
 
 export { findAllUsers, insertUser, findUser, updateUser, deleteUser };
